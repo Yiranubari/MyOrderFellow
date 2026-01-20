18cbee8ea41bdc50f86ba15a845f7f62c255c141
Yiranubari <yiranubari4@gmail.com>
2026-01-20 20:53:10 +0100

    fix: Correctly execute and return user data in findByEmail method

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,5 +31,8 @@ class User
     public function findByEmail($email)
     {
         $sql = "SELECT * FROM users WHERE email = :email LIMIT 1";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([':email' => $email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 }
