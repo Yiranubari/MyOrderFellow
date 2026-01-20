348bc68ef8833166ec8891dce11daeebcb5a9ab9
Yiranubari <yiranubari4@gmail.com>
2026-01-20 20:47:27 +0100

    feat: Implement create method in User class for user registration

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,5 +18,13 @@ class User
     public function create($data)
     {
         $hashedPassword = password_hash($data['password'], PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([
+            ':name' => $data['name'],
+            ':email' => $data['email'],
+            ':password' => $hashedPassword,
+        ]);
+        return $this->conn->lastInsertId();
     }
 }
