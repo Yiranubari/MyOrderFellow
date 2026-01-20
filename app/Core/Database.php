0884907c350a4d72cbc0898923ca41db0e894c67
Yiranubari <yiranubari4@gmail.com>
2026-01-20 12:46:13 +0100

    refactor: Enhance Database class structure and initialization

--- app/Core/Database.php
+++ app/Core/Database.php
@@ -2,8 +2,26 @@
 
 namespace App\Core;
 
-class Database {
-    private $connection;
+use PDO;
+use PDOException;
 
-    public function __const
+
+class Database
+{
+    private $host;
+    private $port;
+    private $database;
+    private $username;
+    private $password;
+
+    public function __construct()
+    {
+        $this->host = $_ENV['DB_HOST'];
+        $this->port = $_ENV['DB_PORT'];
+        $this->database = $_ENV['DB_DATABASE'];
+        $this->username = $_ENV['DB_USERNAME'];
+        $this->password = $_ENV['DB_PASSWORD'];
+
+        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->database};";
+    }
 }
