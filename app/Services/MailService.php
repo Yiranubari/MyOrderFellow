d56796c9565ec8562bde0c5c75aefee5cec9fc85
Yiranubari <yiranubari4@gmail.com>
2026-01-22 06:40:46 +0100

    feat: Implement MailService class for sending OTP emails

--- app/Services/MailService.php
+++ app/Services/MailService.php
@@ -5,3 +5,30 @@ namespace App\Services;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
+
+class MailService
+{
+    private $mail;
+
+    public function __construct()
+    {
+        $this->mail = new PHPMailer(true);
+        $this->mail->isSMTP();
+        $this->mail->Host = $_ENV['SMTP_HOST'];
+        $this->mail->SMTPAuth = true;
+        $this->mail->Username = $_ENV['SMTP_USERNAME'];
+        $this->mail->Password = $_ENV['SMTP_PASSWORD'];
+        $this->mail->Port = $_ENV['SMTP_PORT'];
+    }
+
+    public function sendOTP($recipientEmail, $otp)
+    {
+        $this->mail->setFrom($_ENV['SMTP_USERNAME'], 'My Order Fellow');
+        $this->mail->addAddress($recipientEmail);
+        $this->mail->isHTML(true);
+        $this->mail->Subject = 'Your OTP Code';
+        $this->mail->Body = "<h1>Your OTP Code</h1><p>Your OTP code is: <strong>$otp</strong></p>";
+        $this->mail->send();
+        return true;
+    }
+}
