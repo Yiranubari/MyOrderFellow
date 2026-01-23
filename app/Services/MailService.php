8930c62894b730df4f56dacb3ff484ab26b289f1
Yiranubari <yiranubari4@gmail.com>
2026-01-23 13:51:25 +0100

    fix: Improve SMTP configuration to use getenv for environment variables

--- app/Services/MailService.php
+++ app/Services/MailService.php
@@ -14,18 +14,19 @@ class MailService
     {
         $this->mail = new PHPMailer(true);
         $this->mail->isSMTP();
-        $this->mail->Host = $_ENV['SMTP_HOST'];
+        $this->mail->Host = getenv('SMTP_HOST') ?: $_ENV['SMTP_HOST'] ?? 'sandbox.smtp.mailtrap.io';
         $this->mail->SMTPAuth = true;
-        $this->mail->Username = $_ENV['SMTP_USERNAME'];
-        $this->mail->Password = $_ENV['SMTP_PASSWORD'];
-        $this->mail->Port = $_ENV['SMTP_PORT'];
+        $this->mail->Username = getenv('SMTP_USERNAME') ?: $_ENV['SMTP_USERNAME'] ?? '';
+        $this->mail->Password = getenv('SMTP_PASSWORD') ?: $_ENV['SMTP_PASSWORD'] ?? '';
+        $this->mail->Port = getenv('SMTP_PORT') ?: $_ENV['SMTP_PORT'] ?? 2525;
         $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $this->mail->CharSet = 'UTF-8';
     }
 
     public function sendOTP($recipientEmail, $otp)
     {
-        $this->mail->setFrom($_ENV['SMTP_FROM_EMAIL'], 'My Order Fellow');
+        $fromEmail = getenv('SMTP_FROM_EMAIL') ?: $_ENV['SMTP_FROM_EMAIL'] ?? 'noreply@orderfellow.com';
+        $this->mail->setFrom($fromEmail, 'My Order Fellow');
         $this->mail->addAddress($recipientEmail);
         $this->mail->isHTML(true);
         $this->mail->Subject = 'Your OTP Code';
