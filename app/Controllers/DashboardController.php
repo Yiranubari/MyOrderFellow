eb7497cfb6cdaa1f6c569c43f3445192c1761ac5
Yiranubari <yiranubari4@gmail.com>
2026-01-24 14:24:01 +0100

    feat: Refactor DashboardController to initialize User model in index and generateKey methods

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
+
+
 class DashboardController
 {
     public function index()
     {
+        $userModel = new User();
         session_start();
         if (!isset($_SESSION['company_id'])) {
             header('Location: /login');
             exit();
         }
+        $apiKey = $userModel->getApiKey($_SESSION['company_id']);
         require_once __DIR__ . '/../../views/dashboard/index.php';
+    }
+
+    public function generateKey()
+    {
+        $userModel = new User();
+        session_start();
+        if (!isset($_SESSION['company_id'])) {
+            header('Location: /login');
+            exit();
+        }
     }
 }
