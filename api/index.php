d238486b587d1a1fa563f987a248e5f00715f97e
Yiranubari <yiranubari4@gmail.com>
2026-01-30 03:02:50 +0100

    fix: Add dotenv loading for environment configuration in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
+
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
 require __DIR__ . '/../public/index.php';
